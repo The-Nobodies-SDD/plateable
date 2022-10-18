98becfd7c893bb1591c41d110522f35a12d4d441
Drewszoke <57788297+Drewszoke@users.noreply.github.com>
2022-10-18 17:44:15 -0400

    fixed background image

--- src/containers/Landing.tsx
+++ src/containers/Landing.tsx
@@ -4,7 +4,8 @@ import image from '../assets/landing.jpg'
 
 const Landing = () => {
   return (
-    <div style={{backgroundImage: 'url(' + image + ')'}} >
+    <div style={{backgroundImage: 'url(' + image + ')', height: "100vh", width: "100vw",
+     backgroundPosition: 'center', backgroundSize: "cover", backgroundRepeat: 'no-repeat'}} >
       <h1>Landing Page</h1>
     </div>
   );
