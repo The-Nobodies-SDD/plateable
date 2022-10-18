2be8d2f5f16d04c26a845dcd682b06b2d9b811c5
Drewszoke <57788297+Drewszoke@users.noreply.github.com>
2022-10-18 18:46:27 -0400

    Added all content and sign up button
    Co-authored-by: FeiFei <XyaoFeiFei@users.noreply.github.com>

--- src/containers/Landing.tsx
+++ src/containers/Landing.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import image from '../assets/landing.jpg'
+import { Link } from "react-router-dom"
 //import './styles/containers/landing.scss'
+
+const style = {
+  margin: "auto",
+  padding: "12% 20% 5% 5%",
+  color: "black",
+}
+
+function SignUpButton() {
+  
+  return <Link to="/signup">
+    <button style={{ backgroundColor: 'black', color: 'white'}}>
+      <span style={{ fontSize: "20px" }}>
+        Sign Up
+      </span>
+    </button>
+  </Link>
+}
 
 const Landing = () => {
   return (
     <div style={{backgroundImage: 'url(' + image + ')', height: "100vh", width: "100vw",
-     backgroundPosition: 'center', backgroundSize: "cover", backgroundRepeat: 'no-repeat'}} >
-      <h1>Landing Page</h1>
+     backgroundPosition: 'center', backgroundSize: "cover", backgroundRepeat: 'no-repeat', backgroundBlendMode: "lighten"}} >
+      <h1 style={{fontFamily: "cursive"}}>Plateable</h1>
+        <div style={style}>
+            <h1 style={{ fontWeight: 'bold', fontSize: "60px" }}>Finding recipes has</h1>
+            <h1 style={{ fontWeight: 'bold', fontSize: "60px"}}> never been easier</h1> 
+            <p style={{ fontWeight: 'bold', fontSize: "20px"}}>Generate recipes based on what you have available and keep track of</p>
+            <p style={{ fontWeight: 'bold', fontSize: "20px"}}>what is in your pantry</p>
+            <SignUpButton />
+      </div>
     </div>
   );
 };
