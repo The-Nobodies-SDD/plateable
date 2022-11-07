34f6981b1d1801517f2d997facd94cc6c4519e95
FeiFei <44251254+XyaoFeiFei@users.noreply.github.com>
2022-11-06 20:25:39 -0500

    finalized landing page
    Co-authored-by: Stéfan Martin <martin-stefan@users.noreply.github.com>

--- src/containers/Landing.tsx
+++ src/containers/Landing.tsx
@@ -1,38 +1,21 @@
 import React from 'react';
-import image from '../assets/landing.jpg'
 import Button from 'react-bootstrap/Button';
 import { Link } from "react-router-dom"
-//import './styles/containers/landing.scss'
-
-const style = {
-  margin: "auto",
-  padding: "12% 20% 5% 5%",
-  color: "black",
-}
-
-function SignUpButton() {
-  
-  return <Link to="/signup">
-    <Button variant="outline-dark">
-      <span style={{ fontSize: "20px" }}>
-        Sign Up
-      </span>
-    </Button>
-  </Link>
-}
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 const Landing = () => {
   return (
-    <div style={{backgroundImage: 'url(' + image + ')', height: "100vh", width: "100vw",
-     backgroundPosition: 'center', backgroundSize: "cover", backgroundRepeat: 'no-repeat', backgroundBlendMode: "lighten"}} >
-      <h1 style={{fontFamily: "cursive"}}>Plateable</h1>
-        <div style={style}>
-            <h1 style={{ fontWeight: 'bold', fontSize: "60px" }}>Finding recipes has</h1>
-            <h1 style={{ fontWeight: 'bold', fontSize: "60px"}}> never been easier</h1> 
-            <p style={{ fontWeight: 'bold', fontSize: "20px"}}>Generate recipes based on what you have available and keep track of</p>
-            <p style={{ fontWeight: 'bold', fontSize: "20px"}}>what is in your pantry</p>
-            <SignUpButton />
-      </div>
+    <div className="landing">
+      <h1>Plateable</h1>
+        <div className="hero">
+          <h1 className="display-2 mb-3">Finding recipes has never been easier</h1>
+          <h5 className="mb-4">Generate recipes based on what you have available and keep track of what is in your pantry</h5>
+          
+          <Link to="/login">
+            <Button variant="outline-dark">Sign Up</Button>
+          </Link>
+        </div>
     </div>
   );
 };
