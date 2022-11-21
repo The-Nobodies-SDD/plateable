c16a9b3aa8be7887017f40212e564fe10e93565e
lauren-leee <laurenalee1@yahoo.com>
2022-11-21 13:39:22 -0500

    Implemented MakeRecipe- subtract ingredients from pantry list based on recipe
    Co-authored-by: Stéfan Martin

--- src/containers/Saved.tsx
+++ src/containers/Saved.tsx
@@ -6,6 +6,8 @@ import Recipes from './Recipes';
 import { updateSaved, selectSaved, SavedItem } from '../features/saved/savedSlice';
 import { useAppSelector, useAppDispatch } from '../app/hooks';
 
+import recipeList from '../components/recipes'
+
 /*
 Saved Recipe class:
   presents saved recipe page to the user. 
@@ -23,39 +25,7 @@ const Saved = () => {
   useEffect(() => {
     // placeholder data for saved recipes
     const recipes:SavedItem[] = [
-      {
-        info: {
-          id: "23",
-          title: "Buffalo Chicken Wings",
-          image: "https://cafedelites.com/wp-content/uploads/2017/08/Crispy-Buffalo-Chicken-WIngs-IMAGE-9.jpg",
-          sourceUrl: "",
-          ingredients: [],
-          time: "",
-          instructions: ""
-        }
-      },
-      {
-        info: {
-          id: "213",
-          title: "Roasted Squash ",
-          image: "https://www.melissassouthernstylekitchen.com/wp-content/uploads/2012/07/editedRoastedSquashMedley-Iron-Skillet-Zucchini-Cornbread-064-768x1039.jpg",
-          sourceUrl: "",
-          ingredients: [],
-          time: "",
-          instructions: ""
-        }
-      },
-      {
-        info: {
-          id: "1233",
-          title: "Scalloped Potatoes",
-          image: "https://www.spendwithpennies.com/wp-content/uploads/2018/11/SpendWithPennies-Scalloped-Potatoes-25.jpg",
-          sourceUrl: "",
-          ingredients: [],
-          time: "",
-          instructions: ""
-        }
-      }
+      ...recipeList
     ]
 
     // sets the global saved state when this component is loaded if it is empty
--- src/features/saved/savedSlice.ts
+++ src/features/saved/savedSlice.ts
@@ -1,11 +1,23 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import type { RootState } from '../../app/store'
 
+export type SavedIngredient = {
+  id:string,
+  aisle:string,
+  image:string,
+  name:string,
+  amount:number,
+  unit:string,
+  unitShort:string,
+  unitLong:string,
+  originalString: string
+}
+
 export type SavedItem = {
   info: {
     id: string,
     sourceUrl: string,
-    ingredients: string[],
+    ingredients: SavedIngredient[],
     title: string,
     image: string,
     time: string,
