11e5c253dd3601831be3f0294e0605c2546eddce
lauren-leee <laurenalee1@yahoo.com>
2022-12-01 15:08:01 -0500

    got rid of warnings

--- src/containers/Recipes.tsx
+++ src/containers/Recipes.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+// import ListGroup from 'react-bootstrap/ListGroup';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
 import Recipe from '../components/Recipe';
 import { RecipeProps } from '../App';
 
